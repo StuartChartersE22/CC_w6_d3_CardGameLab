47d574ff25c6b02062e8dc3ee12964c583d97184
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-27 10:48:16 +0100

    card has a suit

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    private Card card;
+
+    @Before
+    public void before(){
+        card = new Card(SuitType.CLUBS, RankType.FOUR);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.CLUBS, card.getSuit());
+    }
+
+    @Test
+
 }
